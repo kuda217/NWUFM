f1c03dfaa067eb0d79a007d8a0cff093c3d3f151
kuda217 <kuda.mawonde@gmail.com>
2016-09-07 03:18:48 +0200

    updated blog with image functionality and removed comments from the blog homepage

---
+++ includes/comments.php
@@ -0,0 +1,27 @@
+<!-- Comments Form -->
+ <div class="well">
+     <h4>Leave a Comment:</h4>
+     <form role="form">
+         <div class="form-group">
+             <textarea class="form-control" rows="3"></textarea>
+         </div>
+         <button type="submit" class="btn btn-primary">Submit</button>
+     </form>
+ </div>
+
+<hr><a href="#"><?php echo $post_comment_count. " Comments "; ?></hr>
+
+ <!-- Comment -->
+
+ <div class="media">
+     <a class="pull-left" href="#">
+         <img class="media-object" src="http://placehold.it/64x64" alt="">
+     </a>
+     <div class="media-body">
+         <h4 class="media-heading">Let the fun begin
+             <small>September 1, 2016 at 2:30 AM</small>
+         </h4>
+         Sleep is an abstract concept.
+
+     </div>
+ </div>
--- index.php
+++ index.php
@@ -52,7 +52,7 @@ include "includes/navigation.php";
                 <hr>
 
                 <!-- Preview Image -->
-                <img class="img-responsive" src="http://placehold.it/900x300" alt="">
+                <img class="img-responsive" src="images/<?php echo $post_image; ?>" alt="">
 
                 <hr>
 
@@ -61,7 +61,7 @@ include "includes/navigation.php";
                 <a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
 
                 <hr>
-                <hr><a href="#"><?php echo $post_comment_count. " Comments "; ?></hr>
+
 
 <?php       }     ?>
 
@@ -69,38 +69,14 @@ include "includes/navigation.php";
 
                 <!-- Blog Comments -->
 
-                <!-- Comments Form -->
-                <div class="well">
-                    <h4>Leave a Comment:</h4>
-                    <form role="form">
-                        <div class="form-group">
-                            <textarea class="form-control" rows="3"></textarea>
-                        </div>
-                        <button type="submit" class="btn btn-primary">Submit</button>
-                    </form>
-                </div>
+
 
                 <hr>
 
                 <!-- Posted Comments -->
-
+              <?php //include "includes/comments.php"; ?>
                 <!-- Comment -->
-
-                <div class="media">
-                    <a class="pull-left" href="#">
-                        <img class="media-object" src="http://placehold.it/64x64" alt="">
-                    </a>
-                    <div class="media-body">
-                        <h4 class="media-heading">Let the fun begin
-                            <small>September 1, 2016 at 2:30 AM</small>
-                        </h4>
-                        Sleep is an abstract concept.
-
-                    </div>
-                </div>
-
-            </div>
-
+</div>
             <!-- Blog Sidebar Widgets Column -->
             <?php include "includes/sidebar.php"; ?>
 <!-- Footer -->
